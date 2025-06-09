6d0270494ba794d3d97beaf3806309c1a7caa206
Amanuel <amaanumararaa@gmail.com>
2025-06-09 13:07:19 +0300

    Enhance Collection and Product management: update related names, add products_count field, and implement collection endpoints with proper CRUD operations.

--- store/serializers.py
+++ store/serializers.py
@@ -5,12 +5,14 @@ from rest_framework import serializers
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
+    
+    products_count = serializers.IntegerField()
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
     def calculate_tax(self, product):
