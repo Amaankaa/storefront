5842e2e9d552b8ed661addd91e627fefa1ba91eb
Amanuel <amaanumararaa@gmail.com>
2025-06-11 12:38:45 +0300

    Add pagination support and enhance ProductViewSet filters: implement DefaultPagination, add search and ordering filters.

--- store/views.py
+++ store/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .models import Product, Collection, OrderItem, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
-from store.filters import ProductFilters
+from .filters import ProductFilters
+from .pagination import DefaultPagination
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilters
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request':self.request}
